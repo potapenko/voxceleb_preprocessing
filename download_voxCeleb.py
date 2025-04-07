966cc1840b1ac6ff435914ea062ded2e38db5621
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2025-04-07 13:59:39 +0200

    loading in 10 threads

--- download_voxCeleb.py
+++ download_voxCeleb.py
@@ -13,6 +13,7 @@ warnings.filterwarnings("ignore")
 import cv2
 import yt_dlp
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 from libs.utilities import make_path
 from preprocess_voxCeleb import extract_frames_opencv, preprocess_frames
@@ -75,10 +76,8 @@ def download_video(video_id, video_path, id_path, fail_video_ids=None, cookies=N
         'outtmpl': video_path,
         'progress_hooks': [my_hook],
     }
-    # If a cookies file is provided, use it.
     if cookies:
         ydl_opts['cookiefile'] = cookies
-    # If cookies-from-browser is provided, pass it along.
     if browser_cookies:
         ydl_opts['cookies_from_browser'] = browser_cookies
 
@@ -104,17 +103,63 @@ def split_in_utterances(video_id, video_path, utterance_files, chunk_folder):
         first_frame, last_frame = utterance['FRAME '].iloc[0], utterance['FRAME '].iloc[-1]
         st = first_frame
         en = last_frame    
-        first_frame = round(first_frame / float(REF_FPS), 3)
-        last_frame = round(last_frame / float(REF_FPS), 3)
+        first_frame_sec = round(first_frame / float(REF_FPS), 3)
+        last_frame_sec = round(last_frame / float(REF_FPS), 3)
         head, tail = os.path.split(utterance_files[i])
         tail = tail.split('.tx')[0]
         chunk_name = os.path.join(chunk_folder, video_id + '#' + tail + '#' + str(st) + '-' + str(en) + '.mp4')
         
-        command_fps = 'ffmpeg -y -i {} -qscale:v 5 -r 25 -threads 1 -ss {} -to {} -strict -2 {} -loglevel quiet'.format(video_path, first_frame, last_frame, chunk_name)
+        command_fps = 'ffmpeg -y -i {} -qscale:v 5 -r 25 -threads 1 -ss {} -to {} -strict -2 {} -loglevel quiet'.format(
+            video_path, first_frame_sec, last_frame_sec, chunk_name)
         os.system(command_fps)
         chunk_videos.append(chunk_name)
 
     return chunk_videos
+
+def process_video(id_index, video_path, output_path, cookies, browser_cookies, fail_video_ids,
+                  extract_frames, preprocessing, delete_mp4, delete_or_frames, landmark_est):
+    video_id = video_path.split(os.sep)[-2]
+    output_path_video = os.path.join(output_path, id_index, video_id)
+    make_path(output_path_video)
+
+    # Check if chunk videos already exist.
+    output_path_chunk_videos = os.path.join(output_path_video, 'chunk_videos')
+    if os.path.exists(output_path_chunk_videos) and glob.glob(os.path.join(output_path_chunk_videos, '*.mp4')):
+        print('URL https://www.youtube.com/watch?v=' + video_id + ' is already loaded')
+        return
+
+    print('Download video id {}. Save to {}'.format(video_id, output_path_video))
+    txt_metadata = sorted(glob.glob(os.path.join(video_path, '*.txt')))
+    mp4_path = os.path.join(output_path_video, '{}.mp4'.format(video_id))
+    if not os.path.exists(mp4_path):
+        success = download_video(video_id, mp4_path, id_index, fail_video_ids=fail_video_ids,
+                                 cookies=cookies, browser_cookies=browser_cookies)
+    else:
+        success = True
+
+    if success:
+        make_path(output_path_chunk_videos)
+        chunk_videos = split_in_utterances(video_id, mp4_path, txt_metadata, output_path_chunk_videos)
+        if delete_mp4:
+            os.system('rm -rf {}'.format(mp4_path))
+
+        extracted_frames_path = os.path.join(output_path_video, 'frames')
+        if extract_frames:
+            extract_frames_opencv(chunk_videos, REF_FPS, extracted_frames_path)
+        if preprocessing:
+            image_files = sorted(glob.glob(os.path.join(extracted_frames_path, '*.png')))
+            if len(image_files) > 0:
+                save_dir = os.path.join(output_path_video, 'frames_cropped')
+                make_path(save_dir)
+                preprocess_frames(args.dataset, output_path_video, extracted_frames_path,
+                                  image_files, save_dir, txt_metadata, landmark_est)
+            else:
+                print('There are no extracted frames on path: {}'.format(extracted_frames_path))
+        if delete_or_frames and os.path.exists(extracted_frames_path):
+            os.system('rm -rf {}'.format(extracted_frames_path))
+    else:
+        print('Error downloading video {}/{}. Deleting folder {}'.format(id_index, video_id, output_path_video))
+        os.system('rm -rf {}'.format(output_path_video))
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -128,14 +173,7 @@ if __name__ == "__main__":
     metadata_path = args.metadata_path
     dataset = args.dataset
 
-    # Determine cookies file if provided, or default to "cookies.txt" if exists.
-    cookies = None
-    if args.cookies:
-        cookies = args.cookies
-    elif os.path.exists("cookies.txt"):
-        cookies = "cookies.txt"
-
-    # Determine browser cookies option if provided.
+    cookies = args.cookies if args.cookies else ("cookies.txt" if os.path.exists("cookies.txt") else None)
     browser_cookies = args.cookies_from_browser
 
     if not cookies and not browser_cookies:
@@ -145,8 +183,7 @@ if __name__ == "__main__":
         print('Please download the metadata for {} dataset'.format(dataset))
         exit()
 
-    ids_path = glob.glob(os.path.join(metadata_path, '*/'))
-    ids_path.sort()
+    ids_path = sorted(glob.glob(os.path.join(metadata_path, '*/')))
     print('{} dataset has {} identities'.format(dataset, len(ids_path)))
 
     print('--Delete original mp4 videos: \t\t{}'.format(delete_mp4))
@@ -154,68 +191,24 @@ if __name__ == "__main__":
     print('--Extract frames from chunk videos: \t{}'.format(extract_frames))
     print('--Preprocess original frames: \t\t{}'.format(preprocessing))
 
+    landmark_est = None
     if preprocessing:
         landmark_est = LandmarksEstimation(type='2D')
 
-    for i, id_path in enumerate(ids_path):
-        id_index = id_path.split('/')[-2]
-        videos_path = glob.glob(os.path.join(id_path, '*/'))
-        videos_path.sort()
-        print('*********************************************************')
-        print('Identity {}/{}: {} videos for {} identity'.format(i, len(ids_path), len(videos_path), id_index))
-        
-        for j, video_path in enumerate(videos_path):
-            print('{}/{} videos'.format(j, len(videos_path)))
-            video_id = video_path.split('/')[-2]
-            output_path_video = os.path.join(output_path, id_index, video_id)
-            make_path(output_path_video)
-            
-            # Check if chunked videos already exist.
-            output_path_chunk_videos = os.path.join(output_path_video, 'chunk_videos')
-            if os.path.exists(output_path_chunk_videos) and glob.glob(os.path.join(output_path_chunk_videos, '*.mp4')):
-                print('URL https://www.youtube.com/watch?v=' + video_id + ' is already loaded')
-                continue
-
-            print('Download video id {}. Save to {}'.format(video_id, output_path_video))
-            
-            txt_metadata = glob.glob(os.path.join(video_path, '*.txt'))
-            txt_metadata.sort()
-
-            mp4_path = os.path.join(output_path_video, '{}.mp4'.format(video_id))
-            if not os.path.exists(mp4_path):
-                success = download_video(video_id, mp4_path, id_index, fail_video_ids=fail_video_ids,
-                                         cookies=cookies, browser_cookies=browser_cookies)
-            else:
-                # Video already exists
-                success = True
-
-            if success:
-                # Split into small videos using the metadata
-                make_path(output_path_chunk_videos)
-                chunk_videos = split_in_utterances(video_id, mp4_path, txt_metadata, output_path_chunk_videos)
-                if delete_mp4:  # Delete original video downloaded from YouTube
-                    command_delete = 'rm -rf {}'.format(mp4_path)
-                    os.system(command_delete)
-
-                extracted_frames_path = os.path.join(output_path_video, 'frames')
-                if extract_frames:
-                    # Run frame extraction
-                    extract_frames_opencv(chunk_videos, REF_FPS, extracted_frames_path)
-                if preprocessing:
-                    # Run preprocessing
-                    image_files = glob.glob(os.path.join(extracted_frames_path, '*.png'))
-                    image_files.sort()
-                    if len(image_files) > 0:
-                        save_dir = os.path.join(output_path_video, 'frames_cropped')
-                        make_path(save_dir)
-                        preprocess_frames(dataset, output_path_video, extracted_frames_path, image_files, save_dir, txt_metadata, landmark_est)
-                    else:
-                        print('There are no extracted frames on path: {}'.format(extracted_frames_path))
-                
-                if delete_or_frames and os.path.exists(extracted_frames_path):  # Delete original frames
-                    command_delete = 'rm -rf {}'.format(extracted_frames_path)
-                    os.system(command_delete)
-            else:
-                print('Error downloading video {}/{}. Deleting folder {}'.format(id_index, video_id, output_path_video))
-                command_delete = 'rm -rf {}'.format(output_path_video)
-                os.system(command_delete)
+    # Create a thread pool executor with maximum 10 threads.
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        for id_path in ids_path:
+            id_index = id_path.split(os.sep)[-2]
+            videos_path = sorted(glob.glob(os.path.join(id_path, '*/')))
+            print('*********************************************************')
+            print('Identity: {} with {} videos'.format(id_index, len(videos_path)))
+            for video_path in videos_path:
+                futures.append(
+                    executor.submit(process_video, id_index, video_path, output_path, cookies,
+                                    browser_cookies, fail_video_ids, extract_frames,
+                                    preprocessing, delete_mp4, delete_or_frames, landmark_est)
+                )
+        # Wait for all tasks to finish.
+        for future in futures:
+            future.result()
